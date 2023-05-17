3aac28a368281398ffdbe516ccf5cbd0213c03a6
Nathan <nathan.prudhomme15@gmail.com>
2023-05-17 14:09:15 +0200

    Fenetre Evaluer

--- src/components/atoms/AddBoutton.tsx
+++ src/components/atoms/AddBoutton.tsx
@@ -1,18 +1,38 @@
-import React from 'react';
-import {StyleSheet, TouchableOpacity } from 'react-native';
+import React, { ReactElement } from 'react';
+import {StyleSheet, TouchableOpacity,View,ViewStyle } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';     
 
-export default function AddBoutton() {
-  const handlePress = () => {
-    // Fonction vide qui s'active lorsque vous cliquez sur le bouton
-    // Vous pouvez ajouter votre logique ou vos actions ici
-  };
-  return (
-    <TouchableOpacity style={styles.button} onPress={handlePress}>
+
+
+type Props = {
+  onPress: () => void;
+  style?: ViewStyle;
+};
+const AddBoutton = ({
+  onPress,
+  style,
+}: Props): ReactElement => (
+  <View style={style}>
+  <TouchableOpacity style={[styles.button,style]} onPress={onPress}>
       <AntDesign name="plus" size={50} color="white" />
     </TouchableOpacity>
-  );
-}
+  </View>
+);
+
+
+export default AddBoutton;
+
+// export default function AddBoutton() {
+//   const handlePress = () => {
+//     // Fonction vide qui s'active lorsque vous cliquez sur le bouton
+//     // Vous pouvez ajouter votre logique ou vos actions ici
+//   };
+//   return (
+//     <TouchableOpacity style={styles.button} onPress={handlePress}>
+//       <AntDesign name="plus" size={50} color="white" />
+//     </TouchableOpacity>
+//   );
+// }
 
 const styles = StyleSheet.create({
   button: {
