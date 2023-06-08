f3b31bad3b564ce05b310634e7239e8ba7641011
barranger-candas.justine <justine.barranger-candas@telecom-st-etienne.fr>
2023-06-08 10:30:25 +0200

    test

---
+++ src/components/molecules/RecapSuivi.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { SafeAreaView, View } from 'react-native';
+import BoxPathologie from '../atoms/BoxPathologie';
+import Symptoms from '@screens/Authenticated/Report/Symptoms';
+import { ScrollView } from 'react-native-gesture-handler';
+
+type Symptome = {
+  id: number;
+  name: string;
+  type: string;
+};
+
+type Pathologie = {
+  id: string;
+  name: string;
+  symptoms: Symptome[];
+};
+
+type Props = {
+  objets: (Pathologie[] | Symptome[]);
+  ischeckeable: boolean;
+};
+
+const DropDownMenu = ({ objets, ischeckeable }: Props) => {
+  const isPathologie = Array.isArray(objets) && objets.length > 0 && 'symptoms' in objets[0];
+  
+
+  
+  return (
+    <SafeAreaView>
+    <ScrollView>
+      {objets.map((objet, index) => {
+        if (isPathologie) {
+          const pathologie = objet as Pathologie;
+          return (
+            <BoxPathologie
+              key={index}
+              objet={pathologie}
+              objets={pathologie.symptoms}
+              ischeckeable={ischeckeable}
+            />
+          );
+        } else {
+          const symptome = objet as Symptome;
+          return (
+            <BoxPathologie
+              key={index}
+              objet={symptome}
+              ischeckeable={ischeckeable}
+            />
+          );
+        }
+      })}
+    </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default DropDownMenu;
