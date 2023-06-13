abec01d7bc5b28b7a423a78eff0fd47f69dcc0b7
barranger-candas.justine <justine.barranger-candas@telecom-st-etienne.fr>
2023-06-13 10:48:01 +0200

    EndSuivi OK

--- src/screens/Authenticated/Suivi.tsx
+++ src/screens/Authenticated/Suivi.tsx
@@ -91,8 +91,8 @@ const Suivi = (): ReactElement => {
           <NewSuivi/></>  : 
         <>
           <Title isDate text={i18n.t('commons.today')+DATE_TODAY} />
-          {user.my_personal_datas.map((pathologie: Pathologie) => 
-            (<RecapSuivi objet={pathologie}/>))}
+          {user.my_personal_datas?user.my_personal_datas.map((pathologie: Pathologie) => 
+            (<RecapSuivi objet={pathologie}/>)):null}
           <Button
             text={i18n.t('commons.newsuivi')}
             onPress={ValidateButtonNewSuiviPressed}
