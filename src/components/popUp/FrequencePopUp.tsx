40e42134e0d476566df1530f62656fdba19bfdc5
Thomas PEZDA <thomas.pezda@etu.univ-telecom-st-etienne.fr>
2023-06-19 15:28:33 +0200

    add modifying freq whe n modify a path

--- src/components/popUp/FrequencePopUp.tsx
+++ src/components/popUp/FrequencePopUp.tsx
@@ -2,29 +2,40 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Button from '@components/atoms/Button';
 import i18n from '@i18n/i18n';
+import { Pathologie } from '@store/types';
+import { Ionicons } from '@expo/vector-icons';
+import layout from '@styles/layout';
+import colors from '@styles/colors';
 // import Symptome from '@store/types';
 
 type AskPopUpProps = {
+  path: Pathologie;
   onClose: () => void;
-//   listeSymptome: Symptome[];
 };
 
-const FreqPopUp = ({ onClose }: AskPopUpProps) => {
+const FreqPopUp = ({ path, onClose }: AskPopUpProps) => {
 
     const yesPressed = (): void => {
         console.log("doSmth");
       };
   return (
     <View style={styles.popUpContainer}>
+      <Ionicons
+        name="ios-arrow-round-back"
+        size={layout.navigation.previousIcon.size}
+        color={colors.black}
+        onPress={onClose}
+        style={{marginLeft:12}}
+      />
         <View>
-        <Text style={styles.text}>{i18n.t('report.endrecord')}</Text>
+        <Text style={styles.text}>{i18n.t('suivi.questionfrequence')}</Text>
       </View>
       {/* <Button style={{backgroundColor:'#EE4483'}}
           text={i18n.t('commons.yes')}
           onPress={yesPressed}
         /> */}
         <Button
-          text={i18n.t('commons.close')}
+          text={i18n.t('commons.validate')}
           onPress={onClose}
         />      
     </View>
